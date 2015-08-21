2b6b647c502ab3b62eb3ab57d79b6e6c7b4130a2
abhi3113 <abhishek.csharp@gmail.com>
2015-08-21 18:55:07 -0400

    New changes in Health Plan Portal and Database
    check-in by Papireddy

---
+++ NEW/MedicalCareSolution/Controllers/HealthPlansController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HealthPlanPortal.Models;
+
+namespace HealthPlanPortal.Controllers
+{
+    public class HealthPlansController : Controller
+    {
+        private HealthPlanDatabaseEntities db = new HealthPlanDatabaseEntities();
+
+        // GET: HealthPlans
+        public ActionResult Index()
+        {
+            return View(db.HealthPlans.ToList());
+        }
+
+        // GET: HealthPlans/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HealthPlan healthPlan = db.HealthPlans.Find(id);
+            if (healthPlan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(healthPlan);
+        }
+
+        // GET: HealthPlans/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HealthPlans/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "HealthPlanId,HealthPlanCode,HealthPlanDescription,PCPrequiredBool,PCPNetworkBool")] HealthPlan healthPlan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.HealthPlans.Add(healthPlan);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(healthPlan);
+        }
+
+        // GET: HealthPlans/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HealthPlan healthPlan = db.HealthPlans.Find(id);
+            if (healthPlan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(healthPlan);
+        }
+
+        // POST: HealthPlans/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "HealthPlanId,HealthPlanCode,HealthPlanDescription,PCPrequiredBool,PCPNetworkBool")] HealthPlan healthPlan)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(healthPlan).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(healthPlan);
+        }
+
+        // GET: HealthPlans/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HealthPlan healthPlan = db.HealthPlans.Find(id);
+            if (healthPlan == null)
+            {
+                return HttpNotFound();
+            }
+            return View(healthPlan);
+        }
+
+        // POST: HealthPlans/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            HealthPlan healthPlan = db.HealthPlans.Find(id);
+            db.HealthPlans.Remove(healthPlan);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ NEW/MedicalCareSolution/Controllers/PreventiveCaresController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HealthPlanPortal.Models;
+
+namespace HealthPlanPortal.Controllers
+{
+    public class PreventiveCaresController : Controller
+    {
+        private HealthPlanDatabaseEntities db = new HealthPlanDatabaseEntities();
+
+        // GET: PreventiveCares
+        public ActionResult Index()
+        {
+            var preventiveCares = db.PreventiveCares.Include(p => p.HealthPlan);
+            return View(preventiveCares.ToList());
+        }
+
+        // GET: PreventiveCares/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PreventiveCare preventiveCare = db.PreventiveCares.Find(id);
+            if (preventiveCare == null)
+            {
+                return HttpNotFound();
+            }
+            return View(preventiveCare);
+        }
+
+        // GET: PreventiveCares/Create
+        public ActionResult Create()
+        {
+            ViewBag.HealthPlanId = new SelectList(db.HealthPlans, "HealthPlanId", "HealthPlanCode");
+            return View();
+        }
+
+        // POST: PreventiveCares/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PreventiveCareId,PhysicalExamLimit,RoutinePediatricCareLimit,ImmunizationLimit,HealthPlanId")] PreventiveCare preventiveCare)
+        {
+            if (ModelState.IsValid)
+            {
+                db.PreventiveCares.Add(preventiveCare);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.HealthPlanId = new SelectList(db.HealthPlans, "HealthPlanId", "HealthPlanCode", preventiveCare.HealthPlanId);
+            return View(preventiveCare);
+        }
+
+        // GET: PreventiveCares/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PreventiveCare preventiveCare = db.PreventiveCares.Find(id);
+            if (preventiveCare == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.HealthPlanId = new SelectList(db.HealthPlans, "HealthPlanId", "HealthPlanCode", preventiveCare.HealthPlanId);
+            return View(preventiveCare);
+        }
+
+        // POST: PreventiveCares/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PreventiveCareId,PhysicalExamLimit,RoutinePediatricCareLimit,ImmunizationLimit,HealthPlanId")] PreventiveCare preventiveCare)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(preventiveCare).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.HealthPlanId = new SelectList(db.HealthPlans, "HealthPlanId", "HealthPlanCode", preventiveCare.HealthPlanId);
+            return View(preventiveCare);
+        }
+
+        // GET: PreventiveCares/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            PreventiveCare preventiveCare = db.PreventiveCares.Find(id);
+            if (preventiveCare == null)
+            {
+                return HttpNotFound();
+            }
+            return View(preventiveCare);
+        }
+
+        // POST: PreventiveCares/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            PreventiveCare preventiveCare = db.PreventiveCares.Find(id);
+            db.PreventiveCares.Remove(preventiveCare);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
